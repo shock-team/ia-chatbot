9db67736afeebd1bf85f0872da52c6c23ee9639b
Santiago Gerhart <gerhartsantiago@gmail.com>
2021-08-10 21:40:23 -0300

    chore: first commit

---
+++ IA.ChatBot.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using AIMLbot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA.ChatBot.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot AI = new Bot();
+            AI.loadSettings();
+            AI.loadAIMLFromFiles(); 
+            AI.isAcceptingUserInput = false;
+
+            User myUser = new User("Santi", AI);
+            AI.isAcceptingUserInput = true;
+
+            while (true)
+            {
+                Console.Write("Me: ");
+                Request r = new Request(Console.ReadLine(), myUser, AI);
+
+                Result res = AI.Chat(r);
+
+                Console.WriteLine("Shock!Bot: " + res.Output);
+            }
+        }
+    }
+}
